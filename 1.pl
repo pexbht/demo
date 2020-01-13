6782a4ef1d9bb0a2812922185372179ac6f0834b
pexbht <59824061+pexbht@users.noreply.github.com>
2020-01-13 15:13:05 +0530

    Add files via upload

---
+++ 1.pl
@@ -0,0 +1,16 @@
+# 1. Write a Perl program to check that a string contains only a certain set of characters 
+# (in this case a-z, A-Z and 0-9).
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "enter string (only a-z, A-Z and 0-9):->> ";
+my $str = <>;
+if($str =~ /^[0-9,a-z,A-Z]+$/){
+    print "$str\n";
+    print "Valid\n";
+} else {
+    print "$str\n";
+	print "Not Valid \n ";
+}
